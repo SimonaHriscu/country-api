8b925ad7708f3635ccc51c3ea9b008ec4069b4f3
Simona <see.moona@yahoo.com>
2020-06-23 20:40:13 +0200

    Updates

---
+++ precache-manifest.21aa4440e518a4a2aa1802722389bdce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fda3bd001cb004588e4354f44720f4f",
+    "url": "/country-API/index.html"
+  },
+  {
+    "revision": "244efcfb7592a2557b78",
+    "url": "/country-API/static/css/main.3178b4e8.chunk.css"
+  },
+  {
+    "revision": "8439cae2be9aad62e7ce",
+    "url": "/country-API/static/js/2.08740962.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country-API/static/js/2.08740962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "244efcfb7592a2557b78",
+    "url": "/country-API/static/js/main.e5d193d9.chunk.js"
+  },
+  {
+    "revision": "95716e0869463b993122",
+    "url": "/country-API/static/js/runtime-main.8d24d726.js"
+  },
+  {
+    "revision": "4b5a50c7c671647b6f31c9f5b567b602",
+    "url": "/country-API/static/media/color.4b5a50c7.png"
+  }
+]);
